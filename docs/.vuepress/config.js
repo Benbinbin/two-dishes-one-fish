7139c7c2693f5ed6f4902b891c69f65af9dbaa5f
Ben <3126319718@qq.com>
2021-07-10 11:10:45 +0800

    add markdown-it plugs to support mark sup sub and update deploy script.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -75,7 +75,10 @@ module.exports = {
     }
   },
   extendsMarkdown: (md) => {
-    md.use(require('@neilsustc/markdown-it-katex'), {output: 'html'})
+    md.use(require('@neilsustc/markdown-it-katex'), { output: 'html' });
+    md.use(require('markdown-it-mark'));
+    md.use(require('markdown-it-sub'));
+    md.use(require('markdown-it-sup'));
   },
   define: {
     __BASE__: "/repo/",
